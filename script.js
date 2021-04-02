7186bbbdce2c9bb0fdd0f3c368b1e0c9e8f8bca1
Yitzchok Kimelman <itchkimel@gmail.com>
2021-04-01 23:18:02 -0400

    Basic WebPage before adding the js

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ const h2 = document.createElement('h2');
 h2.textContent = "This content added by JavaScript";
 
 document.querySelector('body').appendChild(h2);
+h2.addEventListener('click', function() {
+  alert('I was clicked!');
+})
+//////////
+
+const subHeader = document.createElement('h3');
+
+function addingEventListener() {
+  const a = document.getElementsByClassName('a');
+  a.addEventListener('click', function() {
+    alert('I was clicked!');
+  }); 
+}
+
+
