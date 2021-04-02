9af24754f6d3065dd4d4a8d765635f65723b27f6
Yitzchok Kimelman <itchkimel@gmail.com>
2021-04-02 16:22:32 -0400

    Uploaded to askg for help with a few issues

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 const h2 = document.createElement('h2');
 h2.textContent = "This content added by JavaScript";
-
 document.querySelector('body').appendChild(h2);
+
 h2.addEventListener('click', function() {
   alert('I was clicked!');
 })
-//////////
 
-const subHeader = document.createElement('h3');
-
-function addingEventListener() {
-  const a = document.getElementsByClassName('a');
-  a.addEventListener('click', function() {
-    alert('I was clicked!');
-  }); 
+function linkedPageWarning() {
+  const linked = document.querySelectorAll('a[target="_blank"]');
+  linked.addEventListener('click', function() {
+    alert('testing the link');
+  });
 }
-
-
